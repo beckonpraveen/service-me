4da61b54e685b1e800ac90b9572ca14e09a50c5e
Praveenkumar Rajendran <beckonpraveen@gmail.com>
2019-03-22 23:46:14 -0400

    removed footer toggle

--- components/footer.vue
+++ components/footer.vue
@@ -1,8 +1,7 @@
 <template>
 <div>
-  <b-navbar toggleable="lg" type="dark" style="background:rgb(79, 145, 205)">
+  <b-navbar type="dark" style="background:rgb(79, 145, 205)">
     <b-navbar-brand class="font-weight-bold" href="#"></b-navbar-brand>
-    <b-navbar-toggle target="nav_collapse" />
   </b-navbar>
 </div>
 </template>
